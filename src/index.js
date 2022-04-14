207f2d500089cebd5996d59d042378c39a8eb919
nabsternab <hayounn.etd@gmail.com>
2022-04-14 00:51:55 +0000

    Populate HTML from array

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+//DOM Selectors
+const tasksSection = document.querySelector('.task-section');
+
+let tasks = [
+    {
+        description : "Clean Room",
+        completed : false,
+        index : 1,
+    },
+    {
+        description : "Grocery Shopping",
+        completed : false,
+        index : 2,
+    }
+];
+
+// iterate over tasks
+let addTask = () => {
+    tasks.forEach((task) => {
+        tasksSection.innerHTML += `
+        <div class="task-row">
+        <i class="fa-regular fa-square-full"></i>
+        <p>${task.description}</p>
+        <i class="fa-regular fa-pen-to-square"></i>
+        </div>
+        `;
+    })
+};
+
+    //populate HTML list
+   
+addTask();
