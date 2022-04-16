4f39cf7c84cb36de22b66a09a65674e08cee109c
nabsternab <hayounn.etd@gmail.com>
2022-04-16 14:35:10 +0000

    Fix linters

--- modules/function.mjs
+++ modules/function.mjs
@@ -1,25 +1,25 @@
-
 const tasksSection = document.querySelector('.task-section');
 
+// eslint-disable-next-line import/no-mutable-exports
 let tasks = [];
 
 class Task {
-    constructor(task) {
-        this.task = task;
-        this.status = false;
-        this.id = tasks.length + 1;
-    }
+  constructor(task) {
+    this.task = task;
+    this.status = false;
+    this.id = tasks.length + 1;
+  }
 }
 
 // Implement a function for adding a new task (add a new element to the array).
-//1. Create a function that loads items from local storage
+// 1. Create a function that loads items from local storage
 
 const loadTasks = () => {
-    if (localStorage.getItem("tasks") !== null) {
-        let storageTasks = JSON.parse(localStorage.getItem('tasks'));
-        tasks = storageTasks;
-        storageTasks.forEach((task) => {
-            tasksSection.innerHTML += `
+  if (localStorage.getItem('tasks') !== null) {
+    const storageTasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks = storageTasks;
+    storageTasks.forEach((task) => {
+      tasksSection.innerHTML += `
             <div class="task-row">
                 <input type="checkbox" class="status">
                 <input type='text' readonly='readonly' value='${task.task}'>
@@ -33,20 +33,18 @@ const loadTasks = () => {
                 </div>
             </div> 
                 `;
-        });
-    }
-}
-
-
+    });
+  }
+};
 
 const addTask = (e) => {
-    e.preventDefault();
-    let taskSelector = document.querySelector('#task-value');
-    let taskValue = taskSelector.value;
-    let newTask = new Task(taskValue);
-    tasks.push(newTask);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    tasksSection.innerHTML += `
+  e.preventDefault();
+  const taskSelector = document.querySelector('#task-value');
+  const taskValue = taskSelector.value;
+  const newTask = new Task(taskValue);
+  tasks.push(newTask);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  tasksSection.innerHTML += `
     <div class="task-row">
         <input type="checkbox" class="status">
         <input type='text' readonly='readonly' value='${newTask.task}'>
@@ -60,7 +58,7 @@ const addTask = (e) => {
         </div>
     </div> 
     `;
-    document.querySelector('#task-value').value = '';
-}
+  document.querySelector('#task-value').value = '';
+};
 
-export {addTask, loadTasks, tasks};
+export { addTask, loadTasks, tasks };
--- src/index.js
+++ src/index.js
@@ -1,82 +1,75 @@
-import _ from 'lodash';
+/* eslint-disable eqeqeq */
 import './style.css';
-import {addTask, loadTasks, tasks} from '../modules/function.mjs';
+import { addTask, loadTasks } from '../modules/function.mjs';
 
-
-//DOM Selectors
+// DOM Selectors
 const addBtn = document.getElementById('add-btn');
-const removeBtn = document.querySelectorAll('.remove-btn')
-const tasksSection = document.querySelector('.task-section');
 
+const tasksSection = document.querySelector('.task-section');
 
 window.addEventListener('DOMContentLoaded', loadTasks);
 addBtn.addEventListener('click', addTask);
 
-//function to remove the task from the UI
+// function to remove the task from the UI
 const removeBookUI = (e) => {
-        e.parentElement.parentElement.parentElement.remove()
-}
+  e.parentElement.parentElement.parentElement.remove();
+};
 
-//function to remove task
-    //updating the LS & the array of tasks
+// function to remove task
+// updating the LS & the array of tasks
 const updateLS = (id) => {
-    let updated = JSON.parse(localStorage.getItem('tasks'));
-    updated.forEach((task, index) => {
-        if(task.id == id) {
-            updated.splice(index, 1);
-        };
-    });
-    for(let i = 0 ;i < updated.length; i++){
-        updated[i].id = i + 1;
+  const updated = JSON.parse(localStorage.getItem('tasks'));
+  updated.forEach((task, index) => {
+    if (task.id == id) {
+      updated.splice(index, 1);
     }
-    localStorage.setItem('tasks',JSON.stringify(updated));
-    tasks = updated;
-}
+  });
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < updated.length; i++) {
+    updated[i].id = i + 1;
+  }
+  localStorage.setItem('tasks', JSON.stringify(updated));
+};
 
 tasksSection.addEventListener('click', (e) => {
-    if(e.target.classList.contains('remove-btn')){
-        removeBookUI(e.target);
-        updateLS(e.target.getAttribute('data-id'));
-        updateIndex();
-    }
+  if (e.target.classList.contains('remove-btn')) {
+    removeBookUI(e.target);
+    updateLS(e.target.getAttribute('data-id'));
+  }
 
-    if(e.target.classList.contains('check')){
-        console.log(editUI(e.target))
-    }
+  //   if (e.target.classList.contains('check')) {
+  //     console.log(editUI(e.target));
+  //   }
 
-    if(e.target.classList.contains('edit-btn')){
-        editUI(e.target);
-        console.log(e.target.parentElement.parentElement.previousElementSibling);
-    }
+  if (e.target.classList.contains('edit-btn')) {
+    // eslint-disable-next-line no-use-before-define
+    editUI(e.target);
+  }
 });
 
-let fieldValue;
-
-//editing function
-// function to handle UI 
-    const editUI = (e) => {
-    let field = e.parentElement.parentElement.previousElementSibling;
-        field.setAttribute("value","");
-        field.removeAttribute("readonly");
-        field.focus();
-        // e.removeAttribute("class");
-        e.setAttribute("class","fa-solid fa-check check");
-        field.addEventListener('keydown', (ev) => {
-            if(ev.key === "Enter") {
-                field.setAttribute("readonly","readonly");
-                e.setAttribute("class", "edit-btn fa-regular fa-pen-to-square");
-                let index = e.getAttribute('data-id');
-                let arr = JSON.parse(localStorage.getItem('tasks'));
-                arr.forEach((task) => {
-                    if(task.id == index){
-                        task.task = field.value;
-                    }
-                });
-                localStorage.setItem('tasks', JSON.stringify(arr));
-            }
-        });
+// editing function
+// function to handle UI
+const editUI = (e) => {
+  const field = e.parentElement.parentElement.previousElementSibling;
+  field.setAttribute('value', '');
+  field.removeAttribute('readonly');
+  field.focus();
+  // e.removeAttribute("class");
+  e.setAttribute('class', 'fa-solid fa-check check');
+  field.addEventListener('keydown', (ev) => {
+    if (ev.key === 'Enter') {
+      field.setAttribute('readonly', 'readonly');
+      e.setAttribute('class', 'edit-btn fa-regular fa-pen-to-square');
+      const index = e.getAttribute('data-id');
+      const arr = JSON.parse(localStorage.getItem('tasks'));
+      arr.forEach((task) => {
+        if (task.id == index) {
+          task.task = field.value;
+        }
+      });
+      localStorage.setItem('tasks', JSON.stringify(arr));
     }
+  });
+};
 
 // function to update the LS
-
-
